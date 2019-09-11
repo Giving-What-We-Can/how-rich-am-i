db08417323f28471496d296ae3db231a2aa3eb50
Sam Deere <dev@colophonemes.com>
2019-09-11 20:15:38 +0100

    Calculation library does most things we want it to

---
+++ src/lib/calculate/index.js
@@ -0,0 +1,88 @@
+import { currencies } from 'countryinfo'
+import { single as interpolate } from 'simple-interpolation';
+import INCOME_CENTILES from './data/income_centiles.json'
+import PPP_CONVERSION from './data/ppp_conversion.json'
+import EXCHANGE_RATES from './data/exchange_rates.json'
+// we use BigNumber because it's API is nice and we don't have to deal with
+// floating point errors in testing
+import BigNumber from 'bignumber.js'
+
+// data interpolation
+const interpolateIncomeCentile = interpolate(INCOME_CENTILES.map(centile => ({ x: centile.percentage, y: centile.international_dollars })))
+export const interpolateIncomeCentileByAmount = amount =>
+  BigNumber(interpolateIncomeCentile({y: amount}))
+    .decimalPlaces(1)
+    .toNumber()
+
+export const interpolateIncomeAmountByCentile = centile =>
+  BigNumber(interpolateIncomeCentile({ x: centile}))
+    .decimalPlaces(2)
+    .toNumber()
+
+export const MEDIAN_INCOME = interpolateIncomeAmountByCentile(50)
+
+// country code -> currency code lookup
+export const getCurrency = countryCode => currencies(countryCode)[0]
+export const getCurrencyCode = countryCode => getCurrency(countryCode).alphaCode
+
+// calculate how to adjust for household size using OECD equivalised income
+// the weightings are for first adult, subsequent adults and children respectively:
+//   1, 0.7, 0.5
+export const householdEquivalizationFactor = ({adults = 0, children = 0}) =>
+  (
+    (adults === 1
+      ? BigNumber(1)
+      : BigNumber(adults).times(0.7).plus(0.3)
+    ).plus(
+      (BigNumber(children).dividedBy(2))
+    )
+  ).toNumber()
+
+// PPP conversion - returns an amount in I$
+export const internationalizeIncome = (income, countryCode) => BigNumber(income)
+  .dividedBy(PPP_CONVERSION[countryCode].factor)
+  .decimalPlaces(2)
+  .toNumber()
+
+// Exchange rate currency conversion, returns an amount in USD
+export const convertIncome = (income, countryCode) => BigNumber(income)
+  .dividedBy(EXCHANGE_RATES[countryCode].rate)
+  .decimalPlaces(2)
+  .toNumber()
+
+// equivalises an income to a particular household composition
+export const equivalizeIncome = (income, household) => BigNumber(income)
+  .dividedBy(householdEquivalizationFactor(household))
+  .toNumber()
+
+// calculate how many times the median income a person's income is
+export const getMedianMultiple = income => BigNumber(income)
+  .dividedBy(MEDIAN_INCOME)
+  .decimalPlaces(1)
+  .toNumber()
+
+// gold-plated way of multiplying by a decimal
+export const getIncomeAfterDonating = (income, donationPercentage) =>
+  BigNumber(income)
+    .times(BigNumber(100).minus(donationPercentage).dividedBy(100))
+    .decimalPlaces(2)
+    .toNumber()
+
+// the main event. takes an income, country code and household composition,
+// and returns a bunch of useful stats for making comparisons to the
+// rest of the world
+export const calculate = ({ income, countryCode, household}) => {
+  const internationalizedIncome = internationalizeIncome(income, countryCode)
+  const equivalizedIncome = equivalizeIncome(internationalizedIncome, household)
+  const convertedIncome = convertIncome(income, countryCode)
+  const incomeCentile = interpolateIncomeCentileByAmount(equivalizedIncome)
+  const medianMultiple = getMedianMultiple(equivalizedIncome)
+
+  return {
+    internationalizedIncome,
+    equivalizedIncome,
+    convertedIncome,
+    incomeCentile,
+    medianMultiple
+  }
+}
---
+++ src/lib/calculate/index.test.js
@@ -0,0 +1,113 @@
+/* eslint-env jest */
+import {
+  interpolateIncomeCentileByAmount,
+  interpolateIncomeAmountByCentile,
+  MEDIAN_INCOME,
+  getCurrencyCode,
+  householdEquivalizationFactor,
+  internationalizeIncome,
+  equivalizeIncome,
+  convertIncome,
+  getMedianMultiple,
+  getIncomeAfterDonating,
+  calculate
+} from './'
+
+describe('income centile interpolation', () => {
+
+  test('interpolateIncomeCentileByAmount is sane', () => {
+    const centile = interpolateIncomeCentileByAmount(21000)
+    expect(centile).toBeGreaterThan(90)
+  })
+
+  test('interpolateIncomeAmountByCentile is sane', () => {
+    const amount = interpolateIncomeAmountByCentile(95)
+    expect(amount).toBeGreaterThan(25000)
+  })
+
+  test('median income is sane', () => {
+    expect(MEDIAN_INCOME).toBeGreaterThan(2800)
+    expect(MEDIAN_INCOME).toBeLessThan(2900)
+  })
+
+})
+
+describe('currencies', () => {
+  test('currency code lookup is sane', () => {
+    expect(getCurrencyCode('AUS')).toBe('AUD')
+    expect(getCurrencyCode('USA')).toBe('USD')
+    expect(getCurrencyCode('TWN')).toBe('TWD')
+  })
+})
+
+describe('householdEquivalizationFactor', () => {
+  test('one adult', () => {
+    expect(householdEquivalizationFactor({adults: 1})).toBe(1)
+  })
+  test('two adults', () => {
+    expect(householdEquivalizationFactor({adults: 2})).toBe(1.7)
+  })
+  test('three adults', () => {
+    expect(householdEquivalizationFactor({adults: 3})).toBe(2.4)
+  })
+  test('one adult, two children', () => {
+    expect(householdEquivalizationFactor({adults: 1, children: 2})).toBe(2)
+  })
+  test('two adults, five children', () => {
+    expect(householdEquivalizationFactor({ adults: 2, children: 5 })).toBe(4.2)
+  })
+})
+
+describe('internationalise income', () => {
+  test('20 EUR in Germany is sane', () => {
+    const german20EUR = internationalizeIncome(20, 'DEU')
+    expect(german20EUR).toBeGreaterThan(20)
+    expect(german20EUR).toBeLessThan(30)
+  })
+  test('10000 Japanese Yen is sane', () => {
+    const jpy = internationalizeIncome(10000, 'JPN')
+    expect(jpy).toBeGreaterThan(90)
+    expect(jpy).toBeLessThan(110)
+  })
+})
+
+test('equivalizeIncome', () => {
+  // 1 adult 2 children should be an equivalization factor of 2
+  const equivalizedIncome = equivalizeIncome(10000, {adults: 1, children: 2})
+  expect(equivalizedIncome).toBe(5000)
+})
+
+test('getMedianMultiple', () => {
+  expect(getMedianMultiple(30000)).toBe(10.6)
+})
+
+test('getIncomeAfterDonating', () => {
+  expect(getIncomeAfterDonating(30000, 10)).toBe(27000)
+})
+
+describe('convert currency', () => {
+  test('10000 Australian dollars', () => {
+    expect(convertIncome(10000, 'AUS')).toBe(7719.53)
+  })
+})
+
+describe('calculate', () => {
+  test('median household in the UK', () => {
+    const income = 28400
+    const countryCode = 'GBR'
+    const household = {adults: 1, children: 1}
+    const result = calculate({income, countryCode, household})
+    const {
+      internationalizedIncome,
+      equivalizedIncome,
+      convertedIncome,
+      incomeCentile,
+      medianMultiple
+    } = result
+    expect(internationalizedIncome).toBe(41230.05)
+    expect(equivalizedIncome).toBe(27486.7)
+    expect(convertedIncome).toBe(39155.82)
+    expect(incomeCentile).toBe(94.4)
+    expect(medianMultiple).toBe(9.7)
+  })
+})
